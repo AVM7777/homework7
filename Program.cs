c59c836dd5f62968c1f045b865beca81e69868f9
Alexander Markov <foenix89@mail.ru>
2024-04-04 00:10:21 +0300

    решил 1 задачу

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача №1
+Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.*/
+
+using System.Runtime.Intrinsics.Arm;
+
+System.Console.WriteLine("ВВедите число M: ");
+int N = int.Parse(Console.ReadLine());
+System.Console.WriteLine("ВВедите число N: ");
+int M = int.Parse(Console.ReadLine());
+
+void PrintNaturalNumbers(int N, int M)
+{
+    if (1 > N || 1 > M || N > M)
+    {
+        return;
+    }
+    Console.Write(N + " ");
+    PrintNaturalNumbers(N + 1, M);
+}
+PrintNaturalNumbers(N, M);
+{
+    if (1 > N || 1 > M || N < M)
+    {
+        return;
+    }
+    Console.Write(M + " ");
+    PrintNaturalNumbers(M + 1, N);
+}
+
+
+/*Задача №2*/
