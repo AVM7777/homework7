44f7b17cc6523ae54df5e06ab90a8731084a2abe
Alexander Markov <foenix89@mail.ru>
2024-04-04 00:29:24 +0300

    код весь закомментировал

--- obj/Debug/net8.0/Homework7.AssemblyInfo.cs
+++ obj/Debug/net8.0/Homework7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c3329fbfc3650fc12345d39436d034fabe027c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c951283d72f3a9964218bac97ad21939315324f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
